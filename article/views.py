74fb498cbd7e320489da497cd4916ef6d8460001
xiubin zheng <xiubin.zheng7@gmail.com>
2020-07-05 23:47:43 -0400

    fix some html

--- article/views.py
+++ article/views.py
@@ -13,13 +13,11 @@ from django.urls import reverse
 # Create your views here.
 def index(request):
     homepage_article = get_homepage_article()
-    #pprint(homepage_article)
     random_articles = get_random_articles(count=3)
     context = {'homepage_article':homepage_article,
                 'random_articles':random_articles
                 }
 
-    #print(homepage_article[0]['images'])
 
     return render(request,'home.html',context)
 
